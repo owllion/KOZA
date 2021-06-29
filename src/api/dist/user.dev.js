1380822a34f5003ee713a78bbc8ed5591d8ca9b3
Zheng Yu <defrag55345@gmail.com>
2021-06-29 09:40:01 +0800

    modify api http method

--- src/api/dist/user.dev.js
+++ src/api/dist/user.dev.js
@@ -11,139 +11,139 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 
 //login
 var userLogin = function userLogin(data) {
-  return _axios["default"].post('/login', data);
+  return _axios["default"].post("/login", data);
 }; //login captcha
 
 
 exports.userLogin = userLogin;
 
 var loginCaptcha = function loginCaptcha() {
-  return _axios["default"].get('/captcha');
+  return _axios["default"].get("/captcha");
 }; //register
 
 
 exports.loginCaptcha = loginCaptcha;
 
 var userRegister = function userRegister(data) {
-  return _axios["default"].post('/register', data);
-}; //get new token 
+  return _axios["default"].post("/register", data);
+}; //get new token
 
 
 exports.userRegister = userRegister;
 
 var getNewToken = function getNewToken(data) {
-  return _axios["default"].post('/getNewToken', data);
+  return _axios["default"].post("/getNewToken", data);
 }; //logout
 
 
 exports.getNewToken = getNewToken;
 
 var userLogout = function userLogout(data) {
-  return _axios["default"].post('/logout', data);
+  return _axios["default"].post("/logout", data);
 }; //upload avatar
 
 
 exports.userLogout = userLogout;
 
 var upload = function upload(data) {
-  return _axios["default"].post('/user/upload', data);
+  return _axios["default"].post("/user/upload", data);
 }; //deleteAvatar
 
 
 exports.upload = upload;
 
 var deleteAvatar = function deleteAvatar(data) {
-  return _axios["default"].post('/user/upload', data);
+  return _axios["default"].post("/user/upload", data);
 }; //forgotPassword
 
 
 exports.deleteAvatar = deleteAvatar;
 
 var forgotPassword = function forgotPassword(data) {
-  return _axios["default"].post('/forgot-password', data);
+  return _axios["default"].post("/forgot-password", data);
 }; //get userInfo
 
 
 exports.forgotPassword = forgotPassword;
 
 var userInfo = function userInfo() {
-  return _axios["default"].get('/user/me');
-}; //log out all devices 
+  return _axios["default"].get("/user/me");
+}; //log out all devices
 
 
 exports.userInfo = userInfo;
 
 var userLogoutAll = function userLogoutAll() {
-  return _axios["default"].get('/user/logoutAll');
+  return _axios["default"].get("/user/logoutAll");
 }; //modify uesr info
 
 
 exports.userLogoutAll = userLogoutAll;
 
 var userInfoModify = function userInfoModify(data) {
-  return _axios["default"].post('/user/info-modify', data);
-}; //modify user password 
+  return _axios["default"].post("/user/info-modify", data);
+}; //modify user password
 
 
 exports.userInfoModify = userInfoModify;
 
 var userPasswordModify = function userPasswordModify(data) {
-  return _axios["default"].post('/user/password-modify', data);
-}; // add the product to the favorite list 
+  return _axios["default"].post("/user/password-modify", data);
+}; // add the product to the favorite list
 
 
 exports.userPasswordModify = userPasswordModify;
 
 var addToFav = function addToFav(data) {
-  return _axios["default"].post('/user/add/favlist', data);
-}; //remove the product 
+  return _axios["default"].post("/user/add/favlist", data);
+}; //remove the product
 
 
 exports.addToFav = addToFav;
 
 var removeFromFav = function removeFromFav(data) {
-  return _axios["default"].post('/user/delete/favlist', data);
+  return _axios["default"]["delete"]("/user/delete/favlist", data);
 }; //get the cartList
 
 
 exports.removeFromFav = removeFromFav;
 
 var getCart = function getCart() {
-  return _axios["default"].get('/cartList');
+  return _axios["default"].get("/cartList");
 }; //add product(s) to cart
 
 
 exports.getCart = getCart;
 
 var addToCart = function addToCart(data) {
-  return _axios["default"].post('/addToCart', data);
+  return _axios["default"].post("/addToCart", data);
 }; //Update item quantity
 
 
 exports.addToCart = addToCart;
 
 var updateQty = function updateQty(data) {
-  return _axios["default"].post('/cart/product/updateQty', data);
+  return _axios["default"].post("/cart/product/updateQty", data);
 }; //clear cart
 
 
 exports.updateQty = updateQty;
 
 var clearCart = function clearCart() {
-  return _axios["default"]["delete"]('/clearCart');
+  return _axios["default"]["delete"]("/clearCart");
 }; //delete item in cart
 
 
 exports.clearCart = clearCart;
 
 var deleteItemInCart = function deleteItemInCart(data) {
-  return _axios["default"].post('/cart/delete', data);
+  return _axios["default"].post("/cart/delete", data);
 };
 
 exports.deleteItemInCart = deleteItemInCart;
 
 var deleteMultiItem = function deleteMultiItem(data) {
-  return _axios["default"].post('/cart/deleteMany', data);
+  return _axios["default"].post("/cart/deleteMany", data);
 };
 
 exports.deleteMultiItem = deleteMultiItem;
--- src/api/user.js
+++ src/api/user.js
@@ -1,60 +1,65 @@
-import instance from './axios'
+import instance from "./axios";
 
-    //login
-    export const userLogin = data => instance.post('/login', data )
-   
-    //login captcha
-    export const loginCaptcha = () => instance.get('/captcha')
-   
-   //register
-   export const  userRegister = data => instance.post('/register',data)
-   
-   //get new token 
-   export const getNewToken = data => instance.post('/getNewToken', data)
-   
-   //logout
-   export const userLogout = data => instance.post('/logout',data)
-   
-   //upload avatar
-   export const upload = data => instance.post('/user/upload',data)
-   
-   //deleteAvatar
-    export const deleteAvatar = data => instance.post('/user/upload',data)
-   
-    //forgotPassword
-    export const forgotPassword = data => instance.post('/forgot-password',data)
-    
-    //get userInfo
-    export const userInfo = () => instance.get('/user/me')
-   
-    //log out all devices 
-    export const userLogoutAll = () => instance.get('/user/logoutAll')
-  
-    //modify uesr info
-    export const userInfoModify = data => instance.post('/user/info-modify', data)
-   
-    //modify user password 
-    export const userPasswordModify = data => instance.post('/user/password-modify', data)
-   
-    // add the product to the favorite list 
-    export const addToFav = data => instance.post('/user/add/favlist', data)
-   
-    //remove the product 
-    export const removeFromFav = data => instance.post('/user/delete/favlist', data)
-   
-    //get the cartList
-    export const getCart = () => instance.get('/cartList')
-   
-    //add product(s) to cart
-    export const addToCart = data => instance.post('/addToCart', data)
+//login
+export const userLogin = (data) => instance.post("/login", data);
 
-    //Update item quantity
-    export const updateQty = data => instance.post('/cart/product/updateQty', data)
-    
-    //clear cart
-    export const clearCart = () => instance.delete('/clearCart')
-   
-    //delete item in cart
-    export const deleteItemInCart = data => instance.post('/cart/delete', data)
-    
-    export const  deleteMultiItem = data => instance.post('/cart/deleteMany', data)
+//login captcha
+export const loginCaptcha = () => instance.get("/captcha");
+
+//register
+export const userRegister = (data) => instance.post("/register", data);
+
+//get new token
+export const getNewToken = (data) => instance.post("/getNewToken", data);
+
+//logout
+export const userLogout = (data) => instance.post("/logout", data);
+
+//upload avatar
+export const upload = (data) => instance.post("/user/upload", data);
+
+//deleteAvatar
+export const deleteAvatar = (data) => instance.post("/user/upload", data);
+
+//forgotPassword
+export const forgotPassword = (data) => instance.post("/forgot-password", data);
+
+//get userInfo
+export const userInfo = () => instance.get("/user/me");
+
+//log out all devices
+export const userLogoutAll = () => instance.get("/user/logoutAll");
+
+//modify uesr info
+export const userInfoModify = (data) =>
+  instance.post("/user/info-modify", data);
+
+//modify user password
+export const userPasswordModify = (data) =>
+  instance.post("/user/password-modify", data);
+
+// add the product to the favorite list
+export const addToFav = (data) => instance.post("/user/add/favlist", data);
+
+//remove the product
+export const removeFromFav = (data) =>
+  instance.delete("/user/delete/favlist", data);
+
+//get the cartList
+export const getCart = () => instance.get("/cartList");
+
+//add product(s) to cart
+export const addToCart = (data) => instance.post("/addToCart", data);
+
+//Update item quantity
+export const updateQty = (data) =>
+  instance.post("/cart/product/updateQty", data);
+
+//clear cart
+export const clearCart = () => instance.delete("/clearCart");
+
+//delete item in cart
+export const deleteItemInCart = (data) => instance.post("/cart/delete", data);
+
+export const deleteMultiItem = (data) =>
+  instance.post("/cart/deleteMany", data);
